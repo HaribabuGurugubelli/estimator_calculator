282cadad59f301de9d1cd1396ad017f67d553ccb
HaribabuGurugubelli <95918882+HaribabuGurugubelli@users.noreply.github.com>
2022-11-15 18:54:54 -0600

    Adding sample line chart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,62 @@ import * as htmlToImage from "html-to-image";
 import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
 import { jsPDF } from "jspdf";
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
 function App() {
   const [currency, setCurrency] = useState();
   const [income, setIncome] = useState();
@@ -304,8 +360,12 @@ function App() {
         className="btn btn-primary"
         onClick={() => PrintPdf()}
       >
-        Primary
+        Print Result
       </button>
+
+      <div>
+        <Line options={options} data={data} />
+      </div>
     </div>
   );
 }
